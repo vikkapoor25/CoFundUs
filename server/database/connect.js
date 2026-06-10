5b65ec09a65059b42aeaf747d53ef4467e296d2d
zebasaran3 <zebasaran3@email.com>
2026-06-10 14:46:41 +0100

    Add database setup

--- server/database/connect.js
+++ server/database/connect.js
@@ -2,7 +2,9 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 const db = new Pool({
-    connectionString: process.env.DB_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = db;
