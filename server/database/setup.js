1fc757bf517965da0635e6a04149243412c141a2
zebasaran3 <zebasaran3@email.com>
2026-06-10 12:22:56 +0100

    Add changes in setup file

--- server/database/setup.js
+++ server/database/setup.js
@@ -1,16 +1,15 @@
 require("dotenv").config();
 const fs = require("fs");
-const path = require("path");
 const db = require("./connect");
 
-const sql = fs.readFileSync(path.join(__dirname, "setup.sql")).toString();
+const sql = fs.readFileSync("./server/database/setup.sql").toString();
 
 db.query(sql)
-    .then(() => {
-        console.log("Database setup complete.");
-        db.end();
-    })
-    .catch((err) => {
-        console.error("Error running database setup:", error);
-        db.end();
-    });
+  .then(() => {
+    console.log("Database setup complete.");
+    db.end();
+  })
+  .catch((error) => {
+    console.error("Error running database setup:", error);
+    db.end();
+  });
