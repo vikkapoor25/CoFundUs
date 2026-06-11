075842d9851a4ebbeb90b3e894cb538c9ecdf9fb
Vikram Kapoor <vikkapoor@mail.com>
2026-06-11 16:10:02 +0100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const cors = require('cors');
 const householdRouter = require("./routes/household");
 const accountsRouter = require("./routes/accounts");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        name: "",
+        description: ""
+    })
+})
 
 app.use("/user", householdRouter);
 app.use("/bank-accounts", accountsRouter);
