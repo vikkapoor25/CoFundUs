78e9b6c6415f3e1b08186acb8f8b710c39a9d0d7
Tooba52 <t.dagia@hotmail.com>
2026-06-11 11:09:57 +0100

    added styling to navbar

---
+++ client/components/card.jsx
@@ -0,0 +1,13 @@
+import { ScrollView, View, Text, StyleSheet } from 'react-native'
+
+
+export default function card() {
+    
+    <View style={styles.card}>
+        <Text style={styles.cardTitle}>placeholder</Text>
+        <View style={styles.row}>
+            <Text style={styles.label}>placeholder</Text>
+            <Text style={styles.value}>placeholder</Text>
+        </View>
+    </View>
+}
--- client/constants/colours.js
+++ client/constants/colours.js
@@ -2,5 +2,8 @@ export default {
   background: "#eef2f7",
   pageHeader: "#2f4f7a",
   cardTitle: "#4a7ec2",
-  cardBackground: "#fff"
+  cardBackground: "#fff",
+  navBackground: "#4a7ec2",
+  inactiveTab: "#e7e7e7",
+  activeTab: "#fff"
 };
