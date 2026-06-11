25aa0afbb77b60f3dfc1a5a8d6f5737854a3ac6d
vikram kapoor <vikkapoor@mail.com>
2026-06-11 11:20:15 +0100

    Import modules for unit testing

---
+++ server/__tests__/unit/models/Household.test.js
@@ -0,0 +1,4 @@
+// Import Household Model for testing
+const Household = require('../../../models/Household')
+// Import Database Connection for testing
+const db = require('../../../database/connect')
