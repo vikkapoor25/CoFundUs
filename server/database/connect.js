f95ec414ea4e762c8f9da0df517c40c2093b24c1
zebasaran3 <zebasaran3@email.com>
2026-06-12 10:30:02 +0100

    Add initial integration testing for accounts

--- server/database/connect.js
+++ server/database/connect.js
@@ -1,10 +1,17 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
-const db = new Pool({
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
+const isTest = process.env.NODE_ENV === "test";
+
+const db = isTest
+  ? new Pool({
+      connectionString: process.env.DB_TEST_URL,
+      ssl: false,
+    })
+  : new Pool({
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    });
 
 module.exports = db;
