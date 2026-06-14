1c45c1ad6acc81210599573b2c21742f8064fe2c
vikram kapoor <vikkapoor@mail.com>
2026-06-14 20:45:36 +0100

    Complete controllers goals

--- server/controllers/goals.js
+++ server/controllers/goals.js
@@ -1,117 +1,76 @@
-// Imports Bill model
-const Bill = require("../models/Bill");
+// Imports Goal model
+const Goal = require("../models/Goal");
 
-// GET /Bills/household/:household_id 
-async function getAllHouseholdBillsController(req, res) {
+// GET /Goal/household/:household_id
+async function getAllHouseholdGoalsController(req, res) {
     try {
-        // Gets all Bills from a household with getAllHouseholdBills() from models
-        const bills = await Bill.getAllHouseholdBills(req.params.household_id);
+        // Gets all goals from an household with getAllHouseholdBills() from models
+        const goals = await Goal.getAllHouseholdGoals(req.params.household_id);
         // Sends success response
-        res.status(200).json(bills);
+        res.status(200).json(goals);
     } catch (err) {
         // Sends server error response
         res.status(500).json({ error: err.message });
     }
 }
 
-// GET /Bills/bank-account/:account_id
-async function getAllBankAccountBillsController(req, res) {
-    try {
-        // Gets all Bills from an account with getAllBankAccountBills() from models
-        const bills = await Bill.getAllBankAccountBills(req.params.account_id);
-        // Sends success response
-        res.status(200).json(bills);
-    } catch (err) {
-        // Sends server error response
-        res.status(500).json({ error: err.message });
-    }
-}
-
-// POST /Bills
+// POST /Goal
 // Request Body Example:
 //              {
-//                  "account_id": 1,
-//                  "bill_name": "Netflix",
-//                  "bill_amount": 15,
-//                  "bill_due_date": "2026-07-01",
-//                  "category": "Entertainment",
-//                  "category_type": "Subscription",
-//                  "repeat_bill": true,
-//                  "payment_frequency": "Monthly",
-//                  "bill_repeat_date": "2026-08-01"
+//                  "household_id": 1,
+//                  "goal_name": "Purchase Phone",
+//                  "goal_amount": 300,
+//                  "current_value": 100,
+//                  "target_date": "2026-07-14"
 //              }
-// NOTE: Optional Columns -> bill_due_date, category_type, bill_repeat_date
+// NOTE: Optional Columns -> current_value, target_date
 //       Default Values:
-//       - bill_due_date -> today's date
-//       - category_type -> null
-//       - bill_repeat_date -> null
-// NOTE: paid is automatically assigned FALSE by the database
-async function createBillController(req, res) {
+//       - current_value -> 0
+//       - target_date -> null
+async function createGoalController(req, res) {
     try {
-        // Creates bill with createBill() from models
-        const newBill = await Bill.createBill(req.body);
+        // Creates goal with createGoal() from models
+        const newGoal = await Goal.createGoal(req.body);
         // Sends created response
-        res.status(201).json(newBill);
+        res.status(201).json(newGoal);
     } catch (err) {
         // Sends bad request response
         res.status(400).json({ error: err.message });
     }
 }
 
-// PATCH /Bills
+// PATCH /Goal
 // Request Body Example:
 //              {
-//                  "bill_id": 1,
-//                  "bill_name": "Netflix Premium",
-//                  "bill_amount": 20,
-//                  "bill_due_date": "2026-07-15",
-//                  "bill_repeat_date": "2026-08-15"
+//                  "goal_id": 1,
+//                  "goal_name": "Purchase iPhone",
+//                  "goal_amount": 320,
+//                  "current_value": 120,
+//                  "target_date": "2026-08-14"
 //              }
-// NOTE: Optional Columns -> bill_due_date, bill_repeat_date
+// NOTE: Optional Columns -> target_date
 //       Default Values:
-//       - bill_due_date -> today's date
-//       - bill_repeat_date -> null
-async function updateBillController(req, res) {
+//       - target_date -> null
+async function updateGoalController(req, res) {
     try {
-        // Updates bill with updateBill() from models
-        const updateBill = await Bill.updateBill(req.body);
+        // Updates goal with updateGoal() from models
+        const updateGoal = await Goal.updateGoal(req.body);
         // Sends updated response
-        res.status(200).json(updateBill);
+        res.status(200).json(updateGoal);
     } catch (err) {
         res.status(404).json({ error: err.message });
     }
 }
 
-// PATCH /Bills/Paid
+// DELETE /Goal
 // Request Body Example:
 //              {
-//                  "bill_id": 1
+//                  "goal_id": 1
 //              }
-// NOTE: paid is automatically updated to TRUE
-// Response Body Format:
-//              {
-//                  "paid": true
-//              }
-async function billPaidController(req, res) {
+async function deleteGoalController(req, res) {
     try {
-        // Updates paid in specific bill as true with billPaid() from models
-        const updateBillPaid = await Bill.billPaid(req.body);
-        // Sends updated response
-        res.status(200).json(updateBillPaid);
-    } catch (err) {
-        res.status(404).json({ error: err.message });
-    }
-}
-
-// DELETE /Bills
-// Request Body Example:
-//              {
-//                  "bill_id": 1
-//              }
-async function deleteBillController(req, res) {
-    try {
-        // Deletes bill with deleteBill from models
-        const deleteBill = await Bill.deleteBill(req.body);
+        // Deletes goal with deleteGoal() from models
+        await Goal.deleteGoal(req.body);
         // Sends success response with no body
         res.status(204).end();
     } catch (err) {
@@ -120,11 +79,9 @@ async function deleteBillController(req, res) {
 }
 
 // Exports controller functions
-module.exports = { 
-    getAllHouseholdBillsController, 
-    getAllBankAccountBillsController, 
-    createBillController, 
-    updateBillController, 
-    billPaidController, 
-    deleteBillController 
+module.exports = {  
+    getAllHouseholdGoalsController, 
+    createGoalController, 
+    updateGoalController, 
+    deleteGoalController 
 };
