3529335a9fbfd29910010dff4ad1d68054f707b0
vikram kapoor <vikkapoor@mail.com>
2026-06-14 20:31:16 +0100

    Complete Models Goal code

--- server/models/Goal.js
+++ server/models/Goal.js
@@ -1,76 +1,72 @@
 const db = require('../database/connect');
 
-class Bill {
+class Goal {
 
 constructor({ goal_id, household_id, goal_name, goal_amount, current_value, target_date }) {
     this.goal_id = goal_id;
     this.household_id = household_id;
     this.goal_name = goal_name;
-    this.account_id = account_id;
     this.goal_amount = goal_amount;
     this.current_value = current_value;
-    this.target_date = target_date ? new Date(bill_due_date).toLocaleDateString("en-CA") : null; // So it shows as DATE in API and not DATETIME
+    this.target_date = target_date ? new Date(target_date).toLocaleDateString("en-CA") : null; // So it shows as DATE in API and not DATETIME
 }
 
-    // Gets all bills for a household
-    static async getAllHouseholdBills(household_id) {
-
-        // Runs SQL query: Gets all bills for a household by household_id
-        const response = await db.query("SELECT a.household_id, b.account_id, b.bill_id, b.bill_name, b.bill_amount, b.bill_due_date, b.category, b.category_type, b.repeat_bill, b.payment_frequency, b.bill_repeat_date, b.paid FROM bills b INNER JOIN accounts a ON (b.account_id = a.account_id) WHERE a.household_id = $1;", 
+    // Gets all goals for a household
+    static async getAllHouseholdGoals(household_id) {
+        // Runs SQL query: Gets all goals for a household by household_id
+        const response = await db.query("SELECT * FROM goals WHERE household_id = $1;", 
             [household_id]);
-        // Throws error household has no bills
+        // Throws error household has no goals
         if (response.rows.length === 0) {
-            throw new Error("Household currently has no bills.");
+            throw new Error("Household currently has no financial goals.");
         }
-        // Converts database rows into Bill objects
-        return response.rows.map(row => new Bill(row));
+        // Converts database rows into Goal objects
+        return response.rows.map(row => new Goal(row));
     }
 
-    // Creates a new bill using account_id
-    static async createBill(request_body) {
-        const today = new Date().toLocaleDateString("en-CA");
-        // Destructures request body
-        const { account_id, bill_name, bill_amount, bill_due_date = today, category, category_type = null, repeat_bill, payment_frequency, bill_repeat_date = null } = request_body
-        // Checks if bank account exists before adding bill
-        const existingBankAccount = await db.query("SELECT * FROM accounts WHERE account_id = $1", 
-            [account_id]);
-        if (existingBankAccount.rows.length === 1) {
-            // Creates bill for the bank account using account_id
-            const response = await db.query("INSERT INTO bills (account_id, bill_name, bill_amount, bill_due_date, category, category_type, repeat_bill, payment_frequency, bill_repeat_date) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *;", 
-                [account_id, bill_name, bill_amount, bill_due_date, category, category_type, repeat_bill, payment_frequency, bill_repeat_date]);
-            // Returns created bill
-            return new Bill(response.rows[0]);
+    // Creates a new goal using household_id
+    static async createGoal(request_body) {
+        // Destructures request body 
+        const { household_id, goal_name, goal_amount, current_value = 0, target_date = null } = request_body
+        // Checks if household exists before adding bill
+        const existingHousehold = await db.query("SELECT * FROM household WHERE household_id = $1", 
+            [household_id]);
+        if (existingHousehold.rows.length === 1) {
+            // Creates goal for the household using household_id
+            const response = await db.query("INSERT INTO goals (household_id, goal_name, goal_amount, current_value, target_date) VALUES ($1, $2, $3, $4, $5) RETURNING *;", 
+                [household_id, goal_name, goal_amount, current_value, target_date]);
+            // Returns created goal
+            return new Goal(response.rows[0]);
         } else {
-            throw new Error("Unable to create bill for account.");
+            throw new Error("Unable to create goal for household.");
         }
     }
 
-    // Updates a Bill using bill_id
-    static async updateBill(request_body) {
-        const today = new Date().toLocaleDateString("en-CA");
+    // Updates a Goal using goal_id
+    static async updateGoal(request_body) {
         // Destructures request body
-        const { bill_id, bill_name, bill_amount, bill_due_date = today, bill_repeat_date = null } = request_body
-        // Updates capital using SQL UPDATE
-        const response = await db.query("UPDATE bills SET bill_name = $1, bill_amount = $2, bill_due_date = $3, bill_repeat_date = $4 WHERE bill_id = $5 RETURNING *;", 
-            [bill_name, bill_amount, bill_due_date, bill_repeat_date, bill_id ]);
+        const { goal_id, goal_name, goal_amount, current_value, target_date = null } = request_body;
+        // Updates goal using SQL UPDATE
+        const response = await db.query("UPDATE goals SET goal_name = $1, goal_amount = $2, current_value = $3, target_date = $4 WHERE goal_id = $5 RETURNING *;", 
+            [goal_name, goal_amount, current_value, target_date, goal_id]);
         if (response.rows.length !== 1) {
-            throw new Error("Unable to update bill.");
+            throw new Error("Unable to update goal.");
         }
-        return new Bill(response.rows[0]);
+        return new Goal(response.rows[0]);
     }
 
-    // Deletes a Bill using bill_id
-    static async deleteBill(data) {
-        const { bill_id } = data
-        // Deletes bill from database
-        const response = await db.query("DELETE FROM bills WHERE bill_id = $1 RETURNING *;", 
-            [bill_id]);
+    // Deletes a Goal using goal_id
+    static async deleteGoal(request_body) {
+        const { goal_id } = request_body
+        // Deletes goal from database
+        const response = await db.query("DELETE FROM goals WHERE goal_id = $1 RETURNING *;", 
+            [goal_id]);
         if (response.rows.length !== 1) {
-            throw new Error("Unable to delete bill.");
+            throw new Error("Unable to delete goal.");
         }
-        return new Bill(response.rows[0]);
+        return new Goal(response.rows[0]);
     }
 }
 
-// Exports Bill class
-module.exports = Bill;
+// Exports Goal class
+module.exports = Goal;
